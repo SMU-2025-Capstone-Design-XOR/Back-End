ebe533f027985c793e059acafdf69d1d55f273c5
b5gye5m <kbg36961@naver.com>
2025-04-06 20:02:42 +0900

    feat: Add Lombok annotations to SignupRequest and LoginRequest
    - @NoArgsConstructor: Enables default constructor for Spring data binding.
    - @AllArgsConstructor: Allows full-field initialization via constructor.
    - @Builder: Supports builder pattern for flexible object creation.
    - Simplifies DTO class definitions and improves maintainability.

--- src/main/java/com/capstone/xor/dto/LoginRequest.java
+++ src/main/java/com/capstone/xor/dto/LoginRequest.java
@@ -1,8 +1,14 @@
 package com.capstone.xor.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor // 기본 생성자(spring 요청 데이터 바인딩)
+@AllArgsConstructor // 전체 필드 초기화 생성자(빌더에서 사용)
+@Builder // 빌더 패턴
 public class LoginRequest {
     private String username;
     private String password;
--- src/main/java/com/capstone/xor/dto/SignupRequest.java
+++ src/main/java/com/capstone/xor/dto/SignupRequest.java
@@ -1,10 +1,14 @@
 package com.capstone.xor.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SignupRequest {
     private String username;
     private String password;
