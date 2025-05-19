a13981bfadf01b7bac019da15c28c990274a56de
b5gye5m <kbg36961@naver.com>
2025-05-20 06:46:42 +0900

    chore: add TomcatConfig.java for future multi-level subfolder support (currently unused)
    - Introduce TomcatConfig.java in preparation for advanced subfolder (3+ levels) support
    - Not yet activated; will be improved and enabled in a future update

---
+++ src/main/java/com/capstone/xor/config/TomcatConfig.java
@@ -0,0 +1,14 @@
+package com.capstone.xor.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector ->
+                connector.setEncodedSolidusHandling("decode"));
+    }
+}
